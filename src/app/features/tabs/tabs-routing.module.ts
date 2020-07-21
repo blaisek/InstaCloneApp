4e2a16e4169319fe229eac8bb3d1d105cf488b3b
Blaise <blaze.kalonda@gmail.com>
2020-07-21 17:25:33 +0200

    update: add navigation tabs

--- src/app/features/tabs/tabs-routing.module.ts
+++ src/app/features/tabs/tabs-routing.module.ts
@@ -1,8 +1,51 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {TabsComponent} from './containers/tabs/tabs.component';
+import {FeedsComponent} from './containers/feeds/feeds.component';
+import {ProfilsComponent} from './containers/profils/profils.component';
+import {SearchComponent} from './containers/search/search.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: TabsComponent,
+    children: [
+      {
+        path: 'feeds',
+        children: [
+          {
+            path: '',
+            component: FeedsComponent
+          },
+        ]
+      },
+      {
+        path: 'search',
+        children: [
+          {
+            path: '',
+            component: SearchComponent
+          },
+        ]
+      },
+      {
+        path: 'profil',
+        children: [
+          {
+            path: '',
+            component: ProfilsComponent
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: 'feed',
+        pathMatch: 'full'
+      }
+    ]
+  },
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/features/tabs/tabs.module.ts
+++ src/app/features/tabs/tabs.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { IonicModule } from '@ionic/angular';
 import { TabsRoutingModule } from './tabs-routing.module';
+import { FeedsComponent } from './containers/feeds/feeds.component';
+import { SearchComponent } from './containers/search/search.component';
+import { ProfilsComponent } from './containers/profils/profils.component';
+import { TabsComponent } from './containers/tabs/tabs.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [FeedsComponent, SearchComponent, ProfilsComponent, TabsComponent],
   imports: [
     CommonModule,
-    TabsRoutingModule
+    TabsRoutingModule,
+    IonicModule.forRoot()
   ]
 })
 export class TabsModule { }
