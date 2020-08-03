965f4283d1d4288b93a8a26539a41f1183bbf4a5
Blaise <blaze.kalonda@gmail.com>
2020-08-03 20:05:00 +0200

    add search component

--- src/app/features/tabs/containers/search/search.component.ts
+++ src/app/features/tabs/containers/search/search.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { first, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  search$: Observable<any>;
+
+  constructor(
+    private _route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.search$ = this._route.data.pipe(
+      first(),
+      map((data: {feeds?: any[]}) => data?.feeds)
+    );
   }
 
 }
